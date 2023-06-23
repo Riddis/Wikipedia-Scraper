ce38647bafb73f3d38ea44a8ba7f65641e8dd5e3
unknown <sam.hoste@hotmail.com>
2023-06-23 11:09:57 +0200

    Finished the exercise

--- README.md
+++ README.md
@@ -2,19 +2,20 @@
 
 - Repository: `Wikipedia Scrapper`
 - Type of Challenge: `Learning`
-- Duration: `3 days`
-- Submission: `23/06/2023 4:30 PM`
+- Time Spent: `2 days`
 
 - Team challenge : `Solo`
+
+This project was done as part of the AI Boocamp at BeCode.org 
 
 ## The Mission
 
 In this project, we will guide you step by step through the process of:
 
 1. creating a self-contained development environment.
-1. retrieving some information from an API (a website for computers)
-2. leveraging it to scrape a website that does not provide an API
-3. saving the output for later processing
+2. retrieving some information from an API (a website for computers)
+3. leveraging it to scrape a website that does not provide an API
+4. saving the output for later processing
 
 Here we query an API for a list of countries and their past leaders. We then extract and sanitize their short bio from Wikipedia. Finally, we save the data to disk.
 
@@ -23,3 +24,15 @@ This task is often the first (coding) step of a datascience project and you will
 ## Learning objectives
 
 You will study topics such as *scraping*, *data structures*, *regular expressions*, *concurrency* and *file handling*. We will point out useful resources at the appropriate time. 
+
+## Installation
+
+You will need to install the following modules:
+
+1. Requests
+2. BeautifulSoup (bs4)
+
+## Usage
+
+You can run the script by running `python main.py` in the terminal
+It will save the data in the data.json file
---
+++ leaders_scraper.py
@@ -0,0 +1,76 @@
+def get_leaders():
+    """Loops through and returns all the leaders per country"""
+    import requests
+    # Set important URLs
+    cookie_url = "https://country-leaders.onrender.com/cookie"
+    countries_url = "https://country-leaders.onrender.com/countries"
+    leaders_url = "https://country-leaders.onrender.com/leaders"
+    leaders_per_country = {}
+    # Create a global session for all our requests
+    global session  
+    session = requests.Session() 
+
+    # Get a cookie
+    cookies=(session.get(cookie_url)).cookies
+    # Get the list of countries
+    countries = (session.get(countries_url, cookies=cookies)).json()
+    # Loop through the list of countries and get their leaders
+    for country in countries:
+        url = leaders_url+"?country="+country
+        leaders_per_country[country] = (session.get(url, cookies=cookies)).json()
+
+    # Return the list of leadders per country
+    return leaders_per_country
+
+def get_first_paragraph(wikipedia_url):
+    """Fetches the first paragraph of wikipedia and sanitizes the output to a readable format"""
+    from bs4 import BeautifulSoup as soup
+    # Get the leader's wikipedia page
+    url = session.get(wikipedia_url).text
+    wikipedia = soup(url, 'html.parser')
+    import re
+
+    # Get all paragraphs on the page
+    for p in wikipedia.find_all('p'):
+        # Find the first paragraph and clean it up
+        if re.match("(^<p><b>(.*?)<\/b>)", str(p)):
+            # remove tags
+            first_paragraph = re.sub("<.*?>", "", str(p))
+            # remove phonetics
+            first_paragraph = re.sub("\(?\/.*?\;", "(", str(first_paragraph))
+            # remove [n]
+            first_paragraph = re.sub("\[.\]", "", str(first_paragraph))
+            first_paragraph = re.sub("\[.*\](\[.*\])?", "", str(first_paragraph))
+            # remove trailing \n
+            first_paragraph = re.sub("\\n", "", str(first_paragraph))
+            # remove various strings
+            first_paragraph = re.sub(".\(background(.*?)couter\).", "", str(first_paragraph))
+            first_paragraph = re.sub(".(\(?)\.mw(.*?)couter\).", "", str(first_paragraph))
+            first_paragraph = re.sub(" ? .mw(.*?)couter.", "", str(first_paragraph))
+            first_paragraph = re.sub(" Dutch pronunciation: (\[.*\])? ;", "", str(first_paragraph))
+            first_paragraph = re.sub("\(? ?uitspraak", "", str(first_paragraph))
+            first_paragraph = re.sub("\(info \/ uitleg\)\)?", "", str(first_paragraph))
+            # Remove extra whitespaces
+            first_paragraph = re.sub("( ){2,}", " ", str(first_paragraph)) # Not working for some reason :'(
+            return first_paragraph
+            # First paragraph has been found, stop the loop
+            break 
+
+def save (file):
+    """Saves a JSON file to .\data.json"""
+    import json
+    with open('.\data.json', 'w') as file:
+        json.dump(leaders_per_country, file)
+
+# Get all leaders
+leaders_per_country = get_leaders()
+# Loop through countries
+for countries in leaders_per_country:
+    #Loop through leaders per country
+    for leaders in leaders_per_country[countries]:
+        # Get the wikipedia entry
+        url = leaders['wikipedia_url']
+        leaders['info'] = get_first_paragraph(url)
+
+# Save the output to a json file
+save(leaders_per_country)
